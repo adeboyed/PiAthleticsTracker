03a9020620746502be2e3bfba16fc936cd43fa91
David Adeboye <david.adeboye@outlook.com>
2018-04-07 22:39:26 +0100

    Finished client side

--- src/client/run.py
+++ src/client/run.py
@@ -1,4 +1,28 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from common import radio
+from common import radio, lightgate
+import time
+
+'''
+Basically we will send a "pulse" to the server every 5 seconds saying we're alive
+If the lightgate is interuptted then we'll send that message
+
+'''
+MESSAGE_PULSE = "P";
+print("PiAthleticsTracker: Client")
+
+
+
+# Define observer functions
+def handleLightGate( red, green, blue ):
+	radio.sendMessage( red + "," + green + "," + blue )	
+
+def handleMessagesFromServer( message ):
+	#Lol nothing to do
+
+setupReader( handleLightGate );
+
+while True:
+	time.sleep( 5 )
+	radio.sendMessage( MESSAGE_PULSE )
