c3e967b321ffe82c167515095535a58fb3f0f243
David Adeboye <david.adeboye@outlook.com>
2018-04-07 23:29:25 +0100

    Added rpc.py

--- src/website/rpc.py
+++ src/website/rpc.py
@@ -1,1 +1,17 @@
-# For David... :-)
+import socket
+import sys
+import pickle
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 10000)
+
+def readStatus():
+	sock.connect(server_address)
+	
+	data = false
+	while not (data):
+		data = sock.recv(16)
+		connection.close()
+
+	return pickle.load( data )	
