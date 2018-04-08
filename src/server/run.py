d0af48142df0823f8b0a2851cc1837cae8695267
David Adeboye <david.adeboye@outlook.com>
2018-04-08 13:29:22 +0100

    Added more to server script

--- src/server/run.py
+++ src/server/run.py
@@ -7,6 +7,7 @@ import alsaaudio
 import socket
 import sys
 import pickle
+import random
 
 '''
 
@@ -34,6 +35,14 @@ inGame = false
 startGameTime = 0
 lastGameTime = 0
 
+status_socket = socket(AF_INET, SOCK_STREAM)
+status_socket.bind( ('localhost', 8081)  )
+status_socket.listen(5)
+
+race_socket = socket(AF_INET, SOCK_STREAM)
+race_socket.bind( ('localhost', 8082)  )
+race_socket.listen(5)
+
 def millis():
 	return time.time() * 1000
 
@@ -56,25 +65,54 @@ def decrementClientStatus():
 	clientStatus -= 1
 	time.sleep(10)	
 
-def clientHandler( c = None ):
+def startRace():
+	gameStartingSoon = true
+  # Wait 10 seconds more starting race
+  time.sleep( 10 )
+
+  # Play track for ready
+
+  # Wait 5 seconds
+  time.sleep ( 5 )
+
+  # Play track for set
+
+  # Wait between 1 and 3 seconds
+  time.sleep ( random.randint(1, 300) / 3 )
+
+  # Play track for go
+	
+
+	gameStartingSoon = false
+	inGame = true
+	startGameTime = millis()
+
+def statusClientHandler( c = None ):
 	data = {
 		'clientStatus': clientStatus,
 		'lightGateCaptured': lightGateCaptured,
 		'raceInProgress': ( gameStartingSoon or inGame ),
 		'lastRaceTime': inGame 	
 	}
+	pickle.dump(data, output)
 	c.sendto(data, client)
 	c.close();
 
-s = socket(AF_INET, SOCK_STREAM)
-server_address = ('localhost', 10000)
-s.bind( server_address )
-s.listen(5)
+def raceClientHandler( c = None ):
+	data = c.recv(1024)
+	c.close()
+	startRace()
+
+def statusSocket():
+	c, addr = status_socket.accept()
+  Thread(target=statusClientHandler, kwargs={ 'c' : c } ).start()
+
+def raceSocket():
+	c, addr = race_socket.accept()
+  Thread(target=raceClientHandler, kwargs={ 'c' : c } ).start()
 
 # Start all of the threads
 Thread( target=decrementClientStatus ).start()
-
-while True:
-	c, addr = s.accept()
-	Thread(target=clientHandler, kwargs={ 'c' : c } ).start()
+Thread( target=statusSocket ).start()
+Thread( target=raceSocket ).start()
 
