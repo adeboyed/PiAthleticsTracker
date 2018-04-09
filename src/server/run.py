eccbca797ee0d0a96c4cd3552453d53ce67dea90
David Adeboye <david.adeboye@outlook.com>
2018-04-09 14:29:47 +0100

    Change socket import

--- src/server/run.py
+++ src/server/run.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from common import radio
 import time
 from subprocess import call
-import socket
+from socket import *
 import sys
 import pickle
 import random
